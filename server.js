0c8563525b79a6d99f31823ffb6ebce3e3808672
Tassos <lambrou@gmail.com>
2018-01-31 14:20:31 -0800

    Opened up all hosts for CORS

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect('mongodb://localhost/missionconsultants')
 const whitelist = (process.env.NODE_ENV === 'production') ? "www.missionconsultants.io" : "*"
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 }
