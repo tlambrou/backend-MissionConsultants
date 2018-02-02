b9dff4aa30c3fd02de33dac6b9c36362a618443f
Tassos <lambrou@gmail.com>
2018-02-01 23:26:33 -0800

    Hotfix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://localhost/missionconsultants')
 // ALLOW CORS
 const whitelist = (process.env.NODE_ENV === 'production') ? "www.missionconsultants.io" : "*"
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', whitelist);
+  res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
